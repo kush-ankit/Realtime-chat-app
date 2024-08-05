3fbf28f969fc90c824116cfa1ba06babc049cbe1
Ankit kushwaha <er.ankitkush@gmail.com>
2024-08-05 19:58:29 +0530

    socket init

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,20 @@
+"use client"
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
+const socket = io('http://localhost:4000');
+
 export default function Home() {
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    })
+  })
+
   return (
     <main>
-      Hello World
+      <p>connections:{socket.connected}</p>
+      <button onClick={()=>socket.emit('hi','you are good')}>click</button>
     </main>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,47 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+// import express, { Express, Request, Response } from "express";
+// import dotenv from "dotenv";
+// dotenv.config();
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+// const app: Express = express();
+// const port = process.env.PORT || 4000;
+// const server = createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000"
+//   }
+// });
+// app.get("/", (req: Request, res: Response) => {
+//   console.log("request received");
+//   res.send("Express + TypeScript Server");
+// });
 
-dotenv.config();
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`[server]: Server is running at http://localhost:${port}`);
+// });
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+io.on("connection", (socket) => {
+  console.log("Socket connection established", socket.id);
+  socket.on('hi', (data) => {
+    console.log(data);
+
+  })
 });
+
+httpServer.listen(4000);
